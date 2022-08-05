6c3421cee24c2354ce2005a01de8d3a394b00926
HilancoJ <hilanco@gmail.com>
2022-08-05 12:18:46 +0200

    Completed Tutorial, time to start having fun.

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Finances
 Small scale budgeting tool.
 
-## Sub Header
-Discuss more detail.
+## Progress
+[100%] Secure User Authentication.
+[20%] Create all HTML Pages.
+[20%] Create all Python Files for HTML Pages.
+[20%] Optimised Database.
--- main.py
+++ main.py
@@ -2,7 +2,5 @@ from website import create_app
 
 app = create_app()
 
-# Only execute this section if the main.py file is run. [https://www.youtube.com/watch?v=3dHyS1W4TIE&ab_channel=ProgrammingKnowledge]
 if __name__ == '__main__':
-	# debug=True means if there is a change in the code, it will automatically re-run the web server.
 	app.run(debug=True, host="0.0.0.0")
